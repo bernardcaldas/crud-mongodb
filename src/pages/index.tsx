53520058b9a7635636a885e9170c4d08bf4d32b2
bernardoDev <bernardo.acaldas@gmail.com>
2022-01-22 23:01:03 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,107 @@
-import type { NextPage } from 'next'
+import { 
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  Button,
+  IconButton,
+} from "@chakra-ui/react";
 
+import {FaPlus, FaEdit, FaTrash} from 'react-icons/fa';
 
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <h1> Ola NextJs</h1>
-  )
-}
+    <Flex
+    w="100%"
+    maxWidth="100%"
+    mx="auto"
+    direction="column"
+    >
+    <Flex
+      w="100%"
+      h="100%"
+      padding="2rem"
+      bg="teal.500"
+      boxShadow="2xl"
+    >
+      <Heading
+        as="h1"
+        size="xl"
+        color="white"
+      >
+        CRUD NextJs + MongoDb</Heading>
+    </Flex>
+    <Flex
+      justifyContent="space-between"
+      w="100%"
+      h="100%"
+      alignContent="center"
+    >
+      <Text
+        marginTop="2rem"
+        fontSize="xl"
+        paddingLeft="2rem"
+      > WebApp - created using nextJs</Text>
+      <Button
+        marginTop="2rem"
+        marginRight="2rem"
+        colorScheme="teal"
+        size="lg"
+        leftIcon={<FaPlus />}
+      >Add User</Button>
+    </Flex>
 
-export default Home
+      
+      <Box
+        margin="2rem"
+        borderRadius="lg"
+        bg="green.50"
+        
+      >
+        <Table size="md">
+          <Thead>
+            <Tr>
+              <Th>AUTHOR</Th>
+              <Th>EMAIL</Th>
+              <Th>EMPLOYED</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Bernardo Caldas</Td>
+              <Td>bernardo.caldas@gmail.com</Td>
+              <Td>24/02/2021</Td>
+              <Td>
+                <Button 
+                  colorScheme="yellow"
+                  size="sm"
+                  leftIcon={<FaEdit/>}
+                >Edit
+                </Button>
+                <Button
+                 colorScheme="red"
+                  size="sm"
+                  marginLeft="1rem"
+                  leftIcon={<FaTrash />}
+                 >Delete</Button>
+              </Td>
+            </Tr>
+            
+          
+          </Tbody>
+        </Table>
+      </Box>
+
+
+    </Flex>
+  )
+
+}
